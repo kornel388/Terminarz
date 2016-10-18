558af4357ccd6a2f52de7a47ef80049f6813cdea
kornel388 <kornel.r@op.pl>
2016-10-18 09:53:40 +0200

    Edytowana klasa Note

--- src/pl/kornel/App.java
+++ src/pl/kornel/App.java
@@ -1,14 +1,21 @@
 package pl.kornel;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		Date dt = new Date();
-		System.out.println(dt.getHour());
-				
+		Date date = new Date();
+		SimpleDateFormat  formatdate = new SimpleDateFormat("yyyy.MM.dd '/' hh:mm:ss");
+		Scanner sc = new Scanner(System.in);
+		String data = formatdate.format(date);
+		String note = sc.nextLine();
+		
+		Notes note1 = new Notes(note, data);
+		note1.getNote();
 	}
 
-}
+} 
--- src/pl/kornel/Notes.java
+++ src/pl/kornel/Notes.java
@@ -1,6 +1,18 @@
 package pl.kornel;
 
 public class Notes {
-
+	
+	private String note;
+	private String date;
+	
+	public Notes(String note,String date) {
+		this.note = note;
+		this.date = date;
+	}
+	
+	public void getNote(){
 		
+		System.out.println("Treœæ notatki: " + note);
+		System.out.println("Data dodania: " + date);
+	}
 }
